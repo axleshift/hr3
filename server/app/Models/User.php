13818d0cb69cf922f5b7a808836f54cb36cd63b7
krystine19 <ttine953@gmail.com>
2025-03-10 11:34:17 +0800

    fix database

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable
      */
     protected $table = 'users';
     protected $fillable = [
+        'role',
         'employee_id',
         'username',
         'name',
--- server/database/migrations/0001_01_01_000000_create_users_table.php
+++ server/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,11 +15,13 @@ return new class extends Migration
             $table->id();
             $table->string('employee_id')->unique()->nullable();
             $table->string('username')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('name');
             $table->string('role')->default('employee');
             $table->string('email')->unique();
             $table->string('password');
             
+            $table->rememberToken();
             $table->timestamps();
         });
 
