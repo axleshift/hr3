696855ecdaa7b85f996d92eb1773a051407a6f56
krystine19 <ttine953@gmail.com>
2025-03-28 19:55:04 +0800

    try fix error

--- server/app/Models/Leave.php
+++ server/app/Models/Leave.php
@@ -17,31 +17,5 @@ class Leave extends Model
         'pay_rate',
         'leave_balance',
         'leave_used',
-        'is_active',
-        'requires_approval',
-        'description'
     ];
-
-    protected $casts = [
-        'is_active' => 'boolean',
-        'requires_approval' => 'boolean',
-        'pay_rate' => 'decimal:2',
-        'leave_balance' => 'decimal:2',
-        'leave_used' => 'decimal:2'
-    ];
-
-    public function scopeActive($query)
-    {
-        return $query->where('is_active', true);
-    }
-
-    public function scopePaid($query)
-    {
-        return $query->where('type', 'Paid');
-    }
-
-    public function scopeUnpaid($query)
-    {
-        return $query->where('type', 'Unpaid');
-    }
 }
