771bdb82162a5f3dfef503286db23f0a0e5228d1
freight-capstone <ttine953@gmail.com>
2025-04-04 22:41:21 +0000

    Production

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,7 @@
     <title>Freight HR 3</title>
     <link rel="manifest" href="./manifest.json">
     <link rel="shortcut icon" href="./favicon.ico">
-    <script type="module" crossorigin src="./assets/index-BqLRPtOi.js"></script>
+    <script type="module" crossorigin src="./assets/index-CPGsFOKs.js"></script>
     <link rel="stylesheet" crossorigin href="./assets/index-CFdBBhXK.css">
   </head>
   <body>
